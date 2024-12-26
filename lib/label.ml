15d229ae578bcc26cb934b2779c50b3dda7ebb26
smjleo <seojun.lee@oriel.ox.ac.uk>
2024-12-26 21:18:08 +1300

    chore: cleanup

--- lib/label.ml
+++ lib/label.ml
@@ -20,50 +20,41 @@ let to_string { from_participant; to_participant; label } =
   | Some label -> parts ^ "_" ^ label
 ;;
 
-let rec sending_labels participant = function
-  | Ast.End -> LSet.empty
-  | Mu (_var, stype) -> sending_labels participant stype
-  | Variable _var -> LSet.empty
-  | Internal { int_part; int_choices } ->
-    let each_choice (_p, { Ast.ch_label; ch_sort = _; ch_cont }) =
-      let cur =
-        { from_participant = participant
-        ; to_participant = int_part
-        ; label = Some ch_label
-        }
-      in
-      Set.union (LSet.singleton cur) (sending_labels participant ch_cont)
-    in
-    List.map ~f:each_choice int_choices |> LSet.union_list
-  | External { ext_part = _; ext_choices } ->
-    List.map ~f:(fun { Ast.ch_cont; _ } -> sending_labels participant ch_cont) ext_choices
-    |> LSet.union_list
-;;
-
-let rec receiving_labels participant = function
-  | Ast.End -> LSet.empty
-  | Mu (_var, stype) -> receiving_labels participant stype
-  | Variable _var -> LSet.empty
-  | Internal { int_part = _; int_choices } ->
-    List.map
-      ~f:(fun (_p, { Ast.ch_cont; _ }) -> receiving_labels participant ch_cont)
-      int_choices
-    |> LSet.union_list
-  | External { ext_part; ext_choices } ->
-    let each_choice { Ast.ch_label; ch_sort = _; ch_cont } =
-      let cur =
-        { from_participant = ext_part
-        ; to_participant = participant
-        ; label = Some ch_label
-        }
-      in
-      Set.union (LSet.singleton cur) (receiving_labels participant ch_cont)
-    in
-    List.map ~f:each_choice ext_choices |> LSet.union_list
-;;
-
 let in_context_item { Ast.ctx_part; ctx_type } =
-  Set.union (sending_labels ctx_part ctx_type) (receiving_labels ctx_part ctx_type)
+  let rec recurse direction participant = function
+    | Ast.End -> LSet.empty
+    | Mu (_var, stype) -> recurse direction participant stype
+    | Variable _var -> LSet.empty
+    | Internal { int_part; int_choices } ->
+      let each_choice (_p, { Ast.ch_label; ch_sort = _; ch_cont }) =
+        let cur =
+          { from_participant = participant
+          ; to_participant = int_part
+          ; label = Some ch_label
+          }
+        in
+        let rest = recurse direction participant ch_cont in
+        match direction with
+        | `Sending -> Set.union (LSet.singleton cur) rest
+        | `Receiving -> rest
+      in
+      List.map ~f:each_choice int_choices |> LSet.union_list
+    | External { ext_part; ext_choices } ->
+      let each_choice { Ast.ch_label; ch_sort = _; ch_cont } =
+        let cur =
+          { from_participant = ext_part
+          ; to_participant = participant
+          ; label = Some ch_label
+          }
+        in
+        let rest = recurse direction participant ch_cont in
+        match direction with
+        | `Sending -> rest
+        | `Receiving -> Set.union (LSet.singleton cur) rest
+      in
+      List.map ~f:each_choice ext_choices |> LSet.union_list
+  in
+  Set.union (recurse `Sending ctx_part ctx_type) (recurse `Receiving ctx_part ctx_type)
 ;;
 
 let in_context context = List.map ~f:in_context_item context |> LSet.union_list
