6916876f6dea4e3098524626b6e9000b1f4adab6
smjleo <seojun.lee@oriel.ox.ac.uk>
2025-08-25 20:21:40 -0400

    feat: add AST for processes

--- lib/ast.ml
+++ lib/ast.ml
@@ -51,3 +51,45 @@ let sort_to_string = function
   | Str -> "str"
   | Bool -> "bool"
 ;;
+
+type session = session_item list [@@deriving sexp]
+
+and session_item =
+  { sess_part : participant
+  ; sess_process : process
+  }
+
+and process =
+  | Nil
+  | Mu of variable * process
+  | Proc_var of variable
+  | Send of send_desc
+  | Receive of recv_desc list
+  | If_then_else of expr * process * process
+  | Flip of probability * process * process
+
+and send_desc =
+  { send_part : participant
+  ; send_label : label
+  ; send_expr : expr
+  ; send_cont : process
+  }
+
+and recv_desc =
+  { recv_part : participant
+  ; recv_label : label
+  ; recv_var : variable
+  ; recv_cont : process
+  }
+
+and expr =
+  | True
+  | False
+  | Int of int
+  | Expr_var of variable
+  | Or of expr
+  | Neg of expr
+  | Add of expr
+  | Succ of expr
+  | Less_than of expr * expr
+  | Nondeterminism of expr * expr
